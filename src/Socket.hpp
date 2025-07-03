61561eab241dbfd2577d1860db32ed3a021717bd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-03 19:01:31 +0200

    open socket

--- src/Socket.hpp
+++ src/Socket.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
-class Socket {};
+#include <sys/socket.h>
+#include <unistd.h>
+
+class Socket {
+public:
+    Socket(int domain, int type, int protocol) {
+        this->fd = socket(domain, type, protocol);
+        // throw on 0 ?
+    }
+
+    ~Socket() {
+        close(this->fd);
+    }
+
+private:
+    int fd;
+};
