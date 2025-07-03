07512140f4c1fac682141c1967ff4c5495a4e981
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-03 17:43:08 +0200

    format

--- src/Request.hpp
+++ src/Request.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 class Request {
-public:
-	class RequestKind {
-		enum Kind {
-			Get,
-		};
-	};
+   public:
+    class RequestKind {
+        enum Kind {
+            Get,
+        };
+    };
 
-	typedef RequestKind Kind;
+    typedef RequestKind Kind;
 
-	Kind kind;
+    Kind kind;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main() {
-	std::cout << "hi\n";
+    std::cout << "hi\n";
 }
