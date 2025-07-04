94d8e06e0c364c5ef7bf53cad3643bd0a5229ca5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-04 13:56:25 +0200

    sketch std::string getline(int fd)

---
+++ src/getline.cpp
@@ -0,0 +1,26 @@
+#include "getline.h"
+
+#include <map>
+
+typedef std::string::size_type usize;
+
+static const usize bufferSize = 64;
+
+std::string getline(int fd) {
+    static std::map<int, std::string> remainders;
+
+    if (fd < 0) {
+        return "";
+    }
+
+    std::string& rem = remainders[fd];
+    std::string out;
+
+    usize maybeNewline = rem.find('\n');
+    if (maybeNewline != std::string::npos) {
+        out = rem.substr(0, maybeNewline);
+        // rem = rem.subs
+    }
+
+    return out;
+}
