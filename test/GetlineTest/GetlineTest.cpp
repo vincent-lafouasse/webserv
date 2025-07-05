de14bd2dc3c2f2ca22d5b6625ac8038d5327598d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-07-05 14:17:57 +0200

    sketch getline tests

--- test/GetlineTest/GetlineTest.cpp
+++ test/GetlineTest/GetlineTest.cpp
@@ -1,5 +1,20 @@
+#include <unistd.h>
+#include <fcntl.h>
+
 #include "gtest/gtest.h"
 
+#include "getline.h"
+
+struct File {
+    File(const std::string& path): fd(open(path.c_str(), O_RDONLY)) {}
+    File(int fd): fd(fd) {}
+    ~File() { close(fd); }
+    int fd;
+};
+
 TEST(Getline, Dummy) {
+    File f("./main.cpp");
+
+    std::string line;
     SUCCEED();
 }
